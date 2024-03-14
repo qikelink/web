0ce0fa38c9e53f75dee4f10f59170b62047439c3
wisdom nwakaku <nwakakucc@gmail.com>
2024-03-14 17:08:16 +0100

    fixed: changes in the landingPage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -85,7 +85,6 @@ export default function Header() {
           ) : (
             <>
               <LoginDialog />
-              <CreateAccountDialog />
             </>
           )}
         </div>
--- frontend/src/components/HomeSection.jsx
+++ frontend/src/components/HomeSection.jsx
@@ -5,14 +5,13 @@ import { isUserValid } from "../../../backend/src/pocketbase";
 
 const HomeSection = () => {
   return (
-    <div className="min-h-screen font-poppins px-8 w-full">
-      <KeywordBar />
+    <div className="min-h-screen font-poppins w-full">
       {!isUserValid ? (
         <div className="flex flex-row justify-center py-1">
-          <div className="w-1/4 px-3 ">
+          <div className="w-1/4 ">
             <ListSection />
           </div>
-          <div className=" w-3/4 flex justify-center flex-col">
+          <div className=" w-3/4 flex justify-center flex-col px-1">
             <HomeCardSection />
           </div>
         </div>
