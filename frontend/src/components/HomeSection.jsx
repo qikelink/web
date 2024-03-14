30e288064eccba046cd0a10c050413a1c5cafadc
nwakakukaks <winnernwakaku123@gmail.com>
2024-03-14 15:05:53 +0100

    stoked

--- frontend/src/components/HomeSection.jsx
+++ frontend/src/components/HomeSection.jsx
@@ -1,18 +1,26 @@
 import HomeCardSection from "@/utils/HomeCardSection";
 import ListSection from "@/utils/ListSection";
 import React from "react";
+import { isUserValid } from "../../../backend/src/pocketbase";
 
 const HomeSection = () => {
   return (
-    <div className="min-h-screen font-poppins w-full">
-      <div className="flex flex-row justify-center py-1">
-        <div className="w-1/4 ">
-          <ListSection />
+    <div className="min-h-screen font-poppins px-8 w-full">
+      <KeywordBar />
+      {!isUserValid ? (
+        <div className="flex flex-row justify-center py-1">
+          <div className="w-1/4 px-3 ">
+            <ListSection />
+          </div>
+          <div className=" w-3/4 flex justify-center flex-col">
+            <HomeCardSection />
+          </div>
         </div>
-        <div className=" w-3/4 flex justify-center flex-col px-1">
+      ) : (
+        <div className=" w-full flex justify-center flex-col">
           <HomeCardSection />
         </div>
-      </div>
+      )}
     </div>
   );
 };
