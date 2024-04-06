069b2e46f20800dea3fca2c1b0a918151470b093
nwakakukaks <winnernwakaku123@gmail.com>
2024-04-06 23:25:12 +0100

    Fix: dynamic pages error

--- frontend/src/app/api/auth/[...nextauth]/route.js
+++ frontend/src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import { authConfig } from "@/lib/session";
 import NextAuth from "next-auth/next";
 
+export const runtime = 'edge';
+
 const handler = NextAuth(authConfig);
 
 export { handler as GET, handler as POST };
