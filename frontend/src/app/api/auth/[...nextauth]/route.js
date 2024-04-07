4c0930b4aa035c6902169c13e232c5be76579274
nwakakukaks <winnernwakaku123@gmail.com>
2024-04-07 15:53:33 +0100

    Runtime: edge

--- frontend/src/app/api/auth/[...nextauth]/route.js
+++ frontend/src/app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,9 @@
 import { authConfig } from "@/lib/session";
 import NextAuth from "next-auth/next";
 
-export const runtime = 'both';
+export const runtime = 'edge';
 
 const handler = NextAuth(authConfig);
 
 export { handler as GET, handler as POST };
+
