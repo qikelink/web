44b98357b9a5b972aee985741a3e4aee71085049
nwakakukaks <winnernwakaku123@gmail.com>
2024-04-07 16:56:19 +0100

    finally

--- frontend/src/app/[roomId]/page.jsx
+++ frontend/src/app/[roomId]/page.jsx
@@ -18,18 +18,10 @@ import { useRouter } from "next/navigation";
 import { BasicIcons } from "@/utils/BasicIcons";
 import { Button } from "@/components/ui/button";
 
-import { authConfig } from "@/lib/session";
-import NextAuth from "next-auth/next";
-
-// Check if the code is running in a Cloudflare Pages environment
 const isCloudflarePages = process.env.VERCEL === '1';
 
-// Set the runtime accordingly
 export const runtime = isCloudflarePages ? 'edge' : process.env.NODE_ENV === 'development' ? undefined : 'edge';
 
-const handler = NextAuth(authConfig);
-
-export { handler as GET, handler as POST };
 
 export default function Home({ params }) {
   const { setProgress } = useAuth();
--- frontend/src/app/token/route.js
+++ frontend/src/app/token/route.js
@@ -1,17 +1,8 @@
 import { AccessToken, Role } from "@huddle01/server-sdk/auth";
 
-import { authConfig } from "@/lib/session";
-import NextAuth from "next-auth/next";
-
-// Check if the code is running in a Cloudflare Pages environment
 const isCloudflarePages = process.env.VERCEL === '1';
 
-// Set the runtime accordingly
 export const runtime = isCloudflarePages ? 'edge' : process.env.NODE_ENV === 'development' ? undefined : 'edge';
-
-const handler = NextAuth(authConfig);
-
-export { handler as GET, handler as POST };
 
 
 export const dynamic = "force-dynamic";
