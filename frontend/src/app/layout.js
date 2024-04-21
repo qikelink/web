0df0521bc58377d5ac647963ef4ecbdb432ac528
nwakakukaks <winnernwakaku123@gmail.com>
2024-04-21 08:53:11 +0100

    Fix: twitter preview image

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter as FontSans } from "next/font/google";
 
-import { cn } from "@/lib/utils";
+import { cn, constructMetadata } from '@/lib/utils'
 import "./globals.css";
 import { AuthProvider } from "@/contexts/auth-context";
 import { Toaster } from "@/components/ui/toaster";
@@ -14,15 +14,17 @@ const fontSans = FontSans({
   weight: "400",
 });
 
-export const metadata = {
-  title: "Qikelink.com || Democractizing YC office Hour",
-  description: "Connecting You To Your Next Level Growth",
-  icons:
-    "https://bafkreif7fy6ndk7v7zqpmcbsngr5fnohjgvdpappfr7r3c33h6ie7oda7a.ipfs.nftstorage.link/",
-  twitter: {
-    card: "summary_large_image",
-  },
-};
+// export const metadata = {
+//   title: "Qikelink.com || Democractizing YC office Hour",
+//   description: "Connecting You To Your Next Level Growth",
+//   icons:
+//     "https://bafkreif7fy6ndk7v7zqpmcbsngr5fnohjgvdpappfr7r3c33h6ie7oda7a.ipfs.nftstorage.link/",
+//   twitter: {
+//     card: "summary_large_image",
+//   },
+// };
+
+export const metadata = constructMetadata()
 
 export default function RootLayout({ children }) {
   return (
--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,45 @@
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Metadata } from 'next'
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
+}
+
+export function constructMetadata({
+  title = "Qikelink.com || YC office Hour but over video",
+  description = "Qikelink is a startup advisory platform Connecting You To great founders for mentorship.",
+  image = "/opengraph-image.jpg",
+  icons = "https://bafkreif7fy6ndk7v7zqpmcbsngr5fnohjgvdpappfr7r3c33h6ie7oda7a.ipfs.nftstorage.link/",
+  noIndex = false
+} = {}) {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image
+        }
+      ]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@winnerAzubuike & @WisdomN69527"
+    },
+    icons,
+    metadataBase: new URL('https://qikelink.com/'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false
+      }
+    })
+  };
 }
