8a91260b52bfd47e9993db9e7c926d204733b0e5
Wisdom Nwakaku <JesusMac@macs-MacBook-Pro.local>
2024-05-08 20:43:24 +0100

    fix: seo

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -42,8 +42,34 @@ export default function Home() {
     handleLogin();
   }, []);
 
+
+  const jsonLd = {
+    "@context": "http://schema.org",
+    "@type": "WebPage",
+    name: "About Us Page",
+    description:
+      "Information about DeFrankFurt Global and its mission and vision",
+    url: "https://www.defrankfurtglobal.com/",
+    mainEntityOfPage: {
+      "@type": "AboutPage",
+      name: "About DeFrankFurt Global",
+      description: "Learn about DeFrankFurt Global and its mission and vision",
+    },
+    author: {
+      "@type": "Organization",
+      name: "DeFrankFurt Global",
+      description: "Top-quality steel rods and iron sheets supplier",
+      url: "https://www.defrankfurtglobal.com/",
+      logo: "https://bafybeidgj5wql5ko4b3bldr3ldgupgexohhatp5gbpbc6mm4hqxhlrfoxm.ipfs.w3s.link/DeFrankfurt%20Global%20icon.png",
+    },
+  };
+
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       {domLoaded && (
         <main className=" lg:px-4 ">
           <Header />
