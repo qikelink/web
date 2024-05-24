42af546a6b66f95bfc717064ec779fef002af30b
wisdom <nwakakucc@gmail.com>
2024-05-24 16:40:27 +0100

    fix: booklink

--- frontend/src/utils/AI.jsx
+++ frontend/src/utils/AI.jsx
@@ -2,14 +2,9 @@
 const Groq = require("groq-sdk");
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
-  // "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"
   dangerouslyAllowBrowser: true,
 });
-// async function main(questions) {
-//   const chatCompletion = await getGroqChatCompletion(questions);
-//   // Print the completion returned by the LLM.
-//   console.log(chatCompletion.choices[0]?.message?.content || "");
-// }
+
 async function getGroqChatCompletion(questions) {
   return groq.chat.completions.create({
     messages: [
